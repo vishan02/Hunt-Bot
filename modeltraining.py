ac726991882be4c50d5a23d35ab1de95af8e7a86
vishan02 <136582889+vishan02@users.noreply.github.com>
2024-04-18 23:27:45 +0100

    epochs 200 and rename trainingYOLO to modeltraining.py

--- modeltraining.py
+++ modeltraining.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")  # build a new model from scratch n means nano version
 
 # Use the model
-model.train(data=r"C:\Users\visha\Downloads\Schnecken.v8i.yolov8\data.yaml", epochs=10)  # train the model
+model.train(data=r"C:\Users\visha\Downloads\Schnecken.v8i.yolov8\data.yaml", epochs=200)  # train the model
 metrics =model.val()  # evaluate model performance on the validation set
 results =model(r"C:\Users\visha\OneDrive\Desktop\objectdetector\slugtest.png")  # predict on an image
 path = model.export(format="onnx")
